0ecd6272058706879feadfc868159fd97e2b1abc
richie <pmc.computer@yahoo.com>
2017-08-17 21:23:56 +0700

    okay

--- PosPy/App.py
+++ PosPy/App.py
@@ -4,16 +4,9 @@
 import frame.main as begin
 
 class Begin(begin.MyApp):
-
     """Kode ini adalah program utama untuk menjalankan aplikasi"""
-
     def __init__(self, *args, **kwds):
         """Kode ini adalah program utama untuk menjalankan aplikasi"""
         super(Begin, self).__init__(*args, **kwds)
-        self.app = self.StartApp()
-        self.Loop()
-
+        self.app = self.MyApp()
 Begin()
-
-
-
